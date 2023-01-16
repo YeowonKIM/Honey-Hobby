88a0d8735b0b91c6ae09698a988cd1abf26f2ee1
1juuun <potenter11@gmail.com>
2023-01-16 22:46:06 +0900

    #1 상세페이지 댓글 기능

--- app.py
+++ app.py
@@ -1,25 +1,67 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
-app = Flask(__name__)
+from flask import Flask, render_template, request, jsonify
+
+import jwt, datetime, requests
 
 from pymongo import MongoClient
-import certifi
 
-ca = certifi.where()
-client = MongoClient('mongodb+srv://test:sparta@cluster0.oxtkdhc.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://test:sparta@cluster0.fgag4po.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
-@app.route('/')
-def home():
-    return render_template('main.html')
+app = Flask(__name__)
+@app.route('/detail')
+def page_detail():
+    return render_template('detail.html')
 
-@app.route('/mypage')
-def mypage():
-   return "This is my page"
+### 댓글 포스트
+@app.route("/api/comments", methods=["POST"])
+def posting_comments():
+    comment_receive = request.form['comment_give']
+    comment_list = list(db.comments.find({}, {'_id': False}))
+    count = len(comment_list) + 1
 
-# @app.route("/connect")
-# def connect():
-#     # return jsonify({'msg':'연결완료!'})
-#     return redirect(url_for('mypage'))
+    doc = {
+         'num': count,
+         'comment': comment_receive
+    }
+    db.comments.insert_one(doc)
+
+    return jsonify({'msg': '저장되었습니다!'})
+
+# @app.route("/api/comments", methods=["POST"])
+# def posting_comments():
+    # token_receive = request.cookies.get('mytoken')
+    # try:
+    #     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    #
+    #     # 댓글 포스팅하기
+    #     user_info = db.users.find_one({"login-id": payload["id"]})
+    #     hobby_info = db.hobby.find_one({"num": payload["id"]})
+    #     comment_receive = request.form["comment_give"]
+    #
+    #     print(type(date_receive))
+    #     doc = {
+    #         "username": user_info["username"],
+    #         "num": hobby_info["num"],
+    #         "comments": comment_receive
+    #     }
+    #     db.comments.insert_one(doc)
+    #
+    #     return jsonify({"result": "success", 'msg': '포스팅 성공'})
+    #
+    # except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+    #     return redirect(url_for("/detail"))
+
+@app.route("/detail/comments_list", methods=["GET"])
+def comments_list_get():
+    comments_list = list(db.comments.find({}, {'_id': False}))
+    return jsonify({'comments': comments_list})
+
+@app.route("/api/comments/delete", methods=["POST"])
+def comment_delete():
+    comment_receive = request.form['comment_give']
+    db.comments.delete_one({"comment":comment_receive})
+
+    return jsonify({'msg': '삭제되었습니다!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
